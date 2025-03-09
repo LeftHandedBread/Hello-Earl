6c507ea513fee4f43523c718fef2331d52cf19f1
KylerMorgan <kgmorgan2000@gmail.com>
2025-03-09 01:34:15 -0500

    LIGHTHOUSE PUZZLE
    functional

--- Scripts/Game Logic/visible_during_night_lighthouse.gd
+++ Scripts/Game Logic/visible_during_night_lighthouse.gd
@@ -1,4 +1,11 @@
+##This script seemed like it was made to through on anything, but for some reason assumed it was
+##specific to only the lighthouse. i may have un generiscized it. my bad
+
+
+
 extends Node
+
+@onready var fader = $AnimationPlayer
 
 var is_visible = false
 # Called when the node enters the scene tree for the first time.
@@ -18,17 +25,15 @@ func _process(_delta: float) -> void:
 			is_visible = false
 
 func enable_visibility():
+	fader.play("fade")
 	for child in get_all_children(self):
-		if child is MeshInstance3D and !is_in_group("colliders"):
-			child.visible(true)
 		if child is CollisionShape3D:
 			child.set_disabled(false)
 
 
 func disable_visibility():
+	fader.play_backwards("fade")
 	for child in get_all_children(self):
-		if child is MeshInstance3D and !is_in_group("colliders"):
-			child.visible(false)
 		if child is CollisionShape3D:
 			child.set_disabled(true)
 
--- scenes/Level 1/lighthouse-swap.gd
+++ scenes/Level 1/lighthouse-swap.gd
@@ -1,11 +1,21 @@
 extends SpotLight3D
 
-@onready var downwards_light = $SpotLight3D
+@onready var downwards_light = $OmniLight3D
+@onready var collision = $Area3D
 
 func _process(_delta: float) -> void:
 	if GameManager.currentLightType == GameManager.Light.RED:
-		self.visible = false
 		downwards_light.visible = false
+		self.light_color = ("ffffee")
+		collision.set_collision_mask_value(1, false)
+		collision.set_collision_mask_value(2, true)
+		collision.set_collision_layer_value(1, false)
+		collision.set_collision_layer_value(2, true)
 	else:
 		self.visible = true
 		downwards_light.visible = true
+		self.light_color = ("ff0000")
+		collision.set_collision_mask_value(1, true)
+		collision.set_collision_mask_value(2, false)
+		collision.set_collision_layer_value(1, true)
+		collision.set_collision_layer_value(2, false)
